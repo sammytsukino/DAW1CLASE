092848d54813740300e7325156491872d981f0cb
sammytsukino <samy.cabello.g@gmail.com>
2024-10-30 10:27:35 +0100

    changes made

--- EJERCICIOS JAVA 2/ejercicios2/Tetera.java
+++ EJERCICIOS JAVA 2/ejercicios2/Tetera.java
@@ -76,37 +76,29 @@ public class Tetera {
         System.out.println("Tetera 1 - Capacidad Máxima: " + tetera1.getCapacidadMaxima());
         System.out.println("Tetera 1 - Cantidad Actual: " + tetera1.getCantidadActual());
 
-        // Llenar la tetera 1 y verificar la cantidad actual
         tetera1.llenarTe();
         System.out.println("Tetera 1 después de llenar - Cantidad Actual: " + tetera1.getCantidadActual());
 
-        // Servir una taza de 200 c.c. de la tetera 1
         tetera1.servirTaza(200);
         System.out.println("Tetera 1 después de servir una taza de 200 c.c. - Cantidad Actual: " + tetera1.getCantidadActual());
 
-        // Vaciar la tetera 1 y verificar la cantidad actual
         tetera1.vaciarTe();
         System.out.println("Tetera 1 después de vaciar - Cantidad Actual: " + tetera1.getCantidadActual());
 
-        // Crear una tetera con capacidad máxima de 1500 c.c. y cantidad inicial de 1500 c.c.
         Tetera tetera2 = new Tetera(1500);
         System.out.println("\nTetera 2 - Capacidad Máxima: " + tetera2.getCapacidadMaxima());
         System.out.println("Tetera 2 - Cantidad Actual: " + tetera2.getCantidadActual());
 
-        // Servir una taza de 500 c.c. de la tetera 2
         tetera2.servirTaza(500);
         System.out.println("Tetera 2 después de servir una taza de 500 c.c. - Cantidad Actual: " + tetera2.getCantidadActual());
 
-        // Agregar 200 c.c. de té a la tetera 2
         tetera2.agregarTe(200);
         System.out.println("Tetera 2 después de agregar 200 c.c. - Cantidad Actual: " + tetera2.getCantidadActual());
 
-        // Crear una tetera con capacidad máxima de 1200 c.c. y cantidad inicial de 1500 c.c.
         Tetera tetera3 = new Tetera(1200, 1500);
         System.out.println("\nTetera 3 - Capacidad Máxima: " + tetera3.getCapacidadMaxima());
         System.out.println("Tetera 3 - Cantidad Actual (ajustada al máximo): " + tetera3.getCantidadActual());
 
-        // Agregar más té a la tetera 3 para comprobar el límite
         tetera3.agregarTe(100);
         System.out.println("Tetera 3 después de intentar agregar 100 c.c. - Cantidad Actual: " + tetera3.getCantidadActual());
     }
