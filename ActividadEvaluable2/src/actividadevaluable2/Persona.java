dc48e1eccd0f03f655f6a8a62cc943ababe22fe9
sammytsukino <samy.cabello.g@gmail.com>
2024-11-11 10:13:56 +0100

    new files added

--- ActividadEvaluable2/src/actividadevaluable2/Persona.java
+++ ActividadEvaluable2/src/actividadevaluable2/Persona.java
@@ -1,3 +1,5 @@
+package actividadevaluable2;
+
 public class Persona {
 	
     private String nombre;
--- ActividadEvaluable2/src/actividadevaluable2/PruebaPersona.java
+++ ActividadEvaluable2/src/actividadevaluable2/PruebaPersona.java
@@ -1,10 +1,11 @@
+package actividadevaluable2;
 
 public class PruebaPersona {
     public static void main(String[] args) {
         Persona persona1 = new Persona("Miguel Ángel", "Conde Díaz", "38678765Q", 1976);
         Persona persona2 = new Persona("Daniel", "Díaz Pérez", "28898765M", 1990);
         
-        System.out.println("Datos de las personas creadas:");
+        System.out.println("Datos de las personas:");
         System.out.println("------------------------");
         persona1.imprimirXPantalla();
         persona2.imprimirXPantalla();
