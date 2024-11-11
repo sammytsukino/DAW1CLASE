da41acac34eb300bae081db961e0a10531499bd3
sammytsukino <samy.cabello.g@gmail.com>
2024-11-11 21:30:26 +0100

    coche changed

--- ActividadEvaluable2/src/actividadevaluable2/Coche.java
+++ ActividadEvaluable2/src/actividadevaluable2/Coche.java
@@ -1,40 +1,116 @@
 package actividadevaluable2;
 
-enum Combustible {
-    GASOLINA, DIESEL, BIOETANOL, BIODIESEL, GASNATURAL
+enum TipoCombustible {
+    GASOLINA, BIOETANOL, DIESEL, BIODIESEL, GAS_NATURAL
 }
 
-enum Tipo {
-    
+enum TipoAutomovil {
     BERLINA, COMPACTO, FAMILIAR, ADVANCE, SUV
-
 }
 
 enum Color {
     BLANCO, NEGRO, ROSA, ROJO, AMARILLO, VERDE, AZUL, GRIS
 }
 
-
 public class Coche {
-
     private String marca;
-    private String modelo;
-    private int fabricacion;
-    private int cilindrada;
-    private Combustible combustible;
-    private Tipo tipo;
-    private int puertas;
-    private int asientos;
-    private static int velocidadMaxima=120;
+    private int modelo;
+    private double motor;
+    private TipoCombustible tipoCombustible;
+    private TipoAutomovil tipoAutomovil;
+    private int numeroPuertas;
+    private int cantidadAsientos;
+    private static final int VELOCIDAD_MAXIMA = 120;
     private Color color;
-    
-    public Coche(String marca, String modelo, int fabricacion, int cilindrada, int puertas, int asientos, Combustible combustible, Tipo tipo, Color color) {
+    private double velocidadActual;
+
+    public Coche(String marca, int modelo, double motor, TipoCombustible tipoCombustible,
+                 TipoAutomovil tipoAutomovil, int numeroPuertas, int cantidadAsientos, Color color) {
         this.marca = marca;
         this.modelo = modelo;
-        this.fabricacion = fabricacion;
-        this.cilindrada = cilindrada;
-        this.puertas = puertas;
-        this.asientos = asientos;
+        this.motor = motor;
+        this.tipoCombustible = tipoCombustible;
+        this.tipoAutomovil = tipoAutomovil;
+        this.numeroPuertas = numeroPuertas;
+        this.cantidadAsientos = cantidadAsientos;
+        this.color = color;
+        this.velocidadActual = 0;
     }
 
-}
+	public double getVelocidadActual() {
+		return velocidadActual;
+	}
+
+	public void setVelocidadActual(double velocidadActual) {
+		this.velocidadActual = velocidadActual;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public int getModelo() {
+		return modelo;
+	}
+
+	public double getMotor() {
+		return motor;
+	}
+
+	public TipoCombustible getTipoCombustible() {
+		return tipoCombustible;
+	}
+
+	public TipoAutomovil getTipoAutomovil() {
+		return tipoAutomovil;
+	}
+
+	public int getNumeroPuertas() {
+		return numeroPuertas;
+	}
+
+	public int getCantidadAsientos() {
+		return cantidadAsientos;
+	}
+
+	public static int getVelocidadMaxima() {
+		return VELOCIDAD_MAXIMA;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	 public void acelerar(int incremento) {
+	        if (velocidadActual + incremento > VELOCIDAD_MAXIMA) {
+	            System.out.println("¡No se puede superar la velocidad máxima de " + VELOCIDAD_MAXIMA + " km/h!");
+	            velocidadActual = VELOCIDAD_MAXIMA;
+	        } else {
+	            velocidadActual += incremento;
+	            System.out.println("Velocidad actual: " + velocidadActual + " km/h");
+	        }
+	    }
+
+	    public void desacelerar(int decremento) {
+	        if (velocidadActual - decremento < 0) {
+	            System.out.println("¡No se puede tener una velocidad negativa!");
+	            velocidadActual = 0;
+	        } else {
+	            velocidadActual -= decremento;
+	            System.out.println("Velocidad actual: " + velocidadActual + " km/h");
+	        }
+	    }
+	    
+	    public void frenar() {
+	        velocidadActual = 0;
+	        System.out.println("El vehículo se ha detenido completamente");
+	    }
+	    
+	    public void calcularTiempoEstimado(double distancia) {
+	        if(velocidadActual > 0) {
+	            double tiempo = distancia / velocidadActual;
+	            System.out.println("Tiempo estimado: " + tiempo + " horas");
+	        } else {
+	            System.out.println("El vehículo está parado");
+	        }
+	    }
+	}
